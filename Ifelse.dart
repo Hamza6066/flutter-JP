1e0119083299dfab3993db0bc055b9376a85ec0e
hamza6066 <hafizhamzaibrahim@gmail.com>
2024-03-03 16:02:33 +0500

    if

---
+++ Ifelse.dart
@@ -0,0 +1,13 @@
+// if(){} (if statement)
+// while(condition){body}
+//  else {}
+
+void main(){
+  var a = 3;
+  var b = 4;
+  if(a>b) {
+    print('if condition is true');
+    }else{
+      print('if condition is false');
+      }
+}
